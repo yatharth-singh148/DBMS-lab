0470407c2c1b08991d0fe7ebb487b94eb95aab63
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2024-11-20 15:41:33 +0530

    Update week5.sql

--- week5.sql
+++ week5.sql
@@ -11,13 +11,13 @@ insert into department values(03,"Technical","Hyderabad");
 insert into department values(04,"Analysis","Mumbai");
 insert into department values(05,"Production","Chennai");
 insert into department values(06,"Business","Bangalore");
-insert into employee values(1111,"Test 1",001,"2023-04-01",90000,01);
-insert into employee values(1112,"Test 2",001,"2023-04-10",150000,02);
-insert into employee values(1113,"Test 3",002,"2023-03-01",130000,03);
-insert into employee values(1114,"Test 4",001,"2023-06-02",95000,04);
-insert into employee values(1115,"Test 5",001,"2023-03-01",93000,01);
-insert into employee values(1116,"Test 6",002,"2023-04-01",90000,01);
-insert into employee values(1117,"Test 7",001,"2023-04-11",200000,02);
+insert into employee values(1111,"Test 1",001,"2018-04-01",90000,01);
+insert into employee values(1112,"Test 2",001,"2018-04-10",150000,02);
+insert into employee values(1113,"Test 3",002,"2018-03-01",130000,03);
+insert into employee values(1114,"Test 4",001,"2018-06-02",95000,04);
+insert into employee values(1115,"Test 5",001,"2018-03-01",93000,01);
+insert into employee values(1116,"Test 6",002,"2018-04-01",90000,01);
+insert into employee values(1117,"Test 7",001,"2017-04-11",200000,02);
 insert into project values(30,"Bangalore","Data Hive");
 insert into project values(31,"Bangalore","Cyber Space");
 insert into project values(32,"Mysuru","Cyber City");
@@ -32,10 +32,10 @@ insert into assignedTo values(1114,31,"Supervision");
 insert into assignedTo values(1115,32,"Supervision");
 insert into assignedTo values(1116,33,"Consultant");
 insert into assignedTo values(1117,36,"Lead");
-insert into incentives values(1111,"2024-03-05",100000);
-insert into incentives values(1113,"2024-06-07",150000);
-insert into incentives values(1115,"2024-05-08",100000);
-insert into incentives values(1116,"2024-05-09",120000);
+insert into incentives values(1111,"2019-03-05",100000);
+insert into incentives values(1113,"2019-06-07",150000);
+insert into incentives values(1115,"2020-05-08",100000);
+insert into incentives values(1116,"2018-05-09",120000);
 select empno from assignedTo where pno in(select pno from project where ploc = "Bangalore" or "Mysuru" or "Hyderabad");
 select empno from employee where empno not in(select empno from incentives);
 select E.ename, E.empno, A.job_role, d.dname, d.dloc as Dept_loc, p.ploc as Proj_Loc
@@ -74,8 +74,8 @@ select min(hire_date) from employee
 ));
 
 select e.empno, ename, sal from employee e, incentives i where e.empno = i.empno and incentive_amt = (
-select max(incentive_amt) from incentives where incentive_date between "2024-03-05" and "2024-06-07" and incentive_amt < (
-select max(incentive_amt) from Incentives where incentive_date between "2019-01-31" and "2019-01-01"
+select max(incentive_amt) from incentives where incentive_date between "2019-03-05" and "2019-06-07" and incentive_amt < (
+select max(incentive_amt) from Incentives where incentive_date between "2019-03-05" and "2019-06-07"
 ));
 
 select e.empno, e.ename from employee e, employee m
